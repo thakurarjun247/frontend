22e6980837d6ac18a29f831fa63b790d2ffab254
Arjun Thakur MBP <thakurarjun247@gmail.com>
2023-11-12 14:18:06 +0530

    fixed nav visibility with js

--- resources/js/top-nav.js
+++ resources/js/top-nav.js
@@ -2,7 +2,9 @@
 var navlinks=document.getElementById("navlinks");
 function showMenu(){
     navlinks.style.right="0";
+    navlinks.style.display="block";
 }
 function hideMenu(){
-    navlinks.style.right="-250px";
+    // navlinks.style.right="-500px";
+    navlinks.style.display="none";
 }
