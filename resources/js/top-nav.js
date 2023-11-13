99b3e931648762e337907525c542265130894f26
Arjun Thakur MBP <thakurarjun247@gmail.com>
2023-11-13 17:24:43 +0530

    improved effects on hover and select active

--- resources/js/top-nav.js
+++ resources/js/top-nav.js
@@ -1,4 +1,14 @@
+const navItems = document.getElementById('navlinks').querySelectorAll('li');
+const currentURL = window.location.href;
 
+for (const navItem of navItems) {
+  const navLink = navItem.querySelector('a');
+  const navLinkURL = navLink.href;
+
+  if (currentURL === navLinkURL) {
+    navItem.classList.add('active');
+  }
+}
 var navlinks=document.getElementById("navlinks");
 function showMenu(){
     navlinks.style.right="0";
@@ -8,3 +18,5 @@ function hideMenu(){
     // navlinks.style.right="-500px";
     navlinks.style.display="none";
 }
+
+
