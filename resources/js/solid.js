65e574e9430d3dd2a77bb1ebc3d44f8501053b9b
Arjun Thakur MBP <thakurarjun247@gmail.com>
2023-11-22 19:35:42 +0530

    fixed p and div font size of body for both mobile and laptop

--- resources/js/solid.js
+++ resources/js/solid.js
@@ -5,3 +5,14 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.display = "none";
   }
+
+//   // Add an event listener to the body element
+// document.body.addEventListener('click', function() {
+//   // Check if the sidenav is open
+//   if (document.getElementById('mySidenav').style.display !== 'none') {
+//     // Close the sidenav by setting its display property to 'none'
+//     document.getElementById('mySidenav').style.display = 'none';
+//   }
+// });
+
+
